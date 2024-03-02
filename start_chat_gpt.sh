34533618ded4b3299b37b7af5ba6e0a8cea03baa
wudingfeng <wdf.coder@gmail.com>
2024-03-02 21:05:06 +0800

    fix: ssl error

--- start_chat_gpt.sh
+++ start_chat_gpt.sh
@@ -7,7 +7,7 @@ NOW=$(date +"%Y-%m-%d-%H-%M")
 
 # 指定log文件的路径
 LOGFILE="./log/$NOW.log"
-
+export https_proxy=
 # 启动uWSGI服务器
 uwsgi --http 0.0.0.0:5511 --master -p 1 --threads 5 -w app:app --daemonize $LOGFILE
 # 看看log到哪儿
